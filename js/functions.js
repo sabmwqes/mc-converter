ba44305f1ca64ace6e17f17aa31fc3ea1d277a98
sabmwqes <nubesuko7823@gmail.com>
2022-10-30 22:02:01 +0900

    change 関数の整理

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,40 @@
-function convert() {
-    $("#alert").text("");
+function customizedRandom(inputJson, patternString){
+    const numOfKeys = inputJson["meta"]["mode_ext"]["column"]; // 入力譜面のキー数
+    var pattern = Array(numOfKeys).fill().map(e => []); // 配列の要素を[]で初期化
+    for (var i=0 ; i<patternString.length ; i++){
+        try {
+            pattern[parseInt(patternString[i])].push(i);
+        } catch {} // 無効な文字があっても無視
+    }
+    // console.log(pattern);
+    var outputJson = Object.assign({}, inputJson); // オブジェクトの値渡し
+    outputJson["note"] = []; // 譜面クリア
+    const notes = inputJson["note"]; // 参照渡し
+    for (var i=0 ; i<notes.length ; i++ ) {
+        if ("column" in notes[i]) { // 音源指定オブジェクトにはcolumnがない
+            var column = notes[i]["column"];
+            for (var k of pattern[column]){
+                var note = Object.assign({}, notes[i]);
+                note["column"] = k;
+                outputJson["note"].push(note);
+            }
+        } else {
+            var note = Object.assign({}, notes[i]);
+            outputJson["note"].push(note);
+        }
+    }
+    outputJson["meta"]["mode_ext"]["column"] = patternString.length; //キー数設定更新
+    outputJson["meta"]["version"] = "[CR: " + patternString + "]" + outputJson["meta"]["version"]; //差分名更新
+    return outputJson;
+}
 
+function keyToSlide(inputJson){
+
+}
+
+function convert(){
+    $("#alert").text("");
+    
     var option = document.querySelector('input[name=op1]:checked').value;
     var inputText = $("#textInput").val();
     var inputJson;
@@ -11,52 +45,27 @@ function convert() {
         return;
     }
     try {
-        const numOfKeys = inputJson["meta"]["mode_ext"]["column"]; //キー数
         if (option == "4kto6k"){
-
+            outputJson = customizedRandom(inputJson, "001233");
         } else if (option == "keyToSlide"){
-
+            
         } else if (option == "removeSOFLAN"){
-
+            
         } else if (option == "shift"){
-
+            
         } else if (option == "customizedRandom"){
-            var patternString = $("#customPattern").val();
-            var pattern = Array(numOfKeys).fill().map(e => []); //[]で初期化
-            for (var i=0 ; i<patternString.length ; i++){
-                try {
-                    pattern[parseInt(patternString[i])].push(i);
-                } catch {}
-            }
-            // console.log(pattern);
-            var outputJson = Object.assign({}, inputJson); //オブジェクトの値渡し
-            outputJson["note"] = []; //譜面クリア
-            const notes = inputJson["note"]; //参照渡し
-            for (var i=0 ; i<notes.length ; i++ ) {
-                if ("column" in notes[i]) {
-                    var column = notes[i]["column"];
-                    for (var k of pattern[column]){
-                        var note = Object.assign({}, notes[i]);
-                        note["column"] = k;
-                        outputJson["note"].push(note);
-                    }
-                } else {
-                    var note = Object.assign({}, notes[i]);
-                    outputJson["note"].push(note);
-                }
-            }
-            outputJson["meta"]["mode_ext"]["column"] = patternString.length; //キー数設定更新
-            outputJson["meta"]["version"] = "[CR: " + patternString + "]" + outputJson["meta"]["version"]; //差分名更新
-            $("#textOutput").val(JSON.stringify(outputJson));
+            outputJson = customizedRandom(inputJson, $("#customPattern").val());
         } else {
-            $("#alert").text("Something is wrong, I can feel it.");
+            $("#alert").text("Something weird happened with option settings.");
         }
     } catch (e){
         $("#alert").text("This is not a beatmap text.");
         return;
     }
+    $("#textOutput").val(JSON.stringify(outputJson));
     $("#filename").text("推奨ファイル名: " + Math.floor(Date.now() / 1000) + ".mc");
 }
+
 function toggleInfo(){
     $("#alert").text("");
     if ( $("#infoBtn").text() == "Hide"){
@@ -91,7 +100,7 @@ function clearInput(){
     $("#alert").text("");
 }
 
-function copytext() {
+function copytext(){
     var text = $("#textOutput").val();
     navigator.clipboard.writeText(text);
     $(".copyMessage").text("Copied!");
